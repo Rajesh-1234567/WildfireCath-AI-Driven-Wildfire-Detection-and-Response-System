9988fa49c70d997f84adcbde56f0812a2ea9b52f
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-03 11:01:09 +0530

    addded team model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+def user_directory_path(instance,filename):
+    return 'user_{0}/{1}'.format(instance,filename)
+
+class TeamModel(models.Model):
+    name=models.CharField(max_length=100)
+    branch=models.CharField(max_length=100)
+    year=models.CharField(max_length=100)
+    role=models.CharField(max_length=100)
+    image=models.ImageField(upload_to=user_directory_path,default="default.png")
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from core.models import TeamModel
 
 def welcome(request):
+    teamdetails=TeamModel.objects.all()
     context={
-
+        'teamdetails':teamdetails,
     }
     return render(request,'core/app/welcome.html',context)

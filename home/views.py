199af8203ca10c64eff25960dda3842aba4400b3
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-04 14:02:22 +0530

    added mapple mapofindia image fetching 350x350

--- home/views.py
+++ home/views.py
@@ -11,6 +11,43 @@ def unix_to_human_readable(unix_time):
     return datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
 #----------------------------------------------------------------------------
 
+#----------------------------------------------------------------------------
+# import io
+# import requests
+# from django.shortcuts import render, redirect
+# from .models import Map
+
+# def generate_map_image(latitude, longitude):
+#     api_key = 'YOUR_GOOGLE_MAPS_API_KEY'
+#     url = f"https://maps.googleapis.com/maps/api/staticmap?center={latitude},{longitude}&zoom=14&size=350x350&maptype=satellite&key={api_key}"
+    
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         image_io = io.BytesIO(response.content)
+#         return image_io
+#     else:
+#         raise Exception("Error fetching map image")
+
+import io
+import requests
+from django.shortcuts import render, redirect
+from .models import Map
+
+def generate_mappls_image(latitude, longitude):
+    api_key = '9440dc184957cb68062acbd0187f20f5'
+    url = f"https://apis.mapmyindia.com/advancedmaps/v1/{api_key}/still_image?center={latitude},{longitude}&zoom=14&size=350x350&maptype=hybrid"
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        image_io = io.BytesIO(response.content)
+        return image_io
+    else:
+        raise Exception("Error fetching map image")
+
+#----------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------
+
 
 @csrf_exempt
 def loading(request):
@@ -18,6 +55,12 @@ def loading(request):
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
         weather_data=get_weather_data(latitude,longitude)
+
+        map_image_io = generate_mappls_image(latitude, longitude)
+            
+        # Create and save Map instance
+        map_instance = Map()
+        map_instance.map_image.save(f"map_{latitude}_{longitude}.png", map_image_io, save=True)
         # print(latitude)
         # print(longitude)
         request.session['weather_data'] = weather_data

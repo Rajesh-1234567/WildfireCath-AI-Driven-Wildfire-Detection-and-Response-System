2f6adbbf995c7adfc5fdec513da9b82808d12d2b
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-04 12:25:54 +0530

    added chatbot

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ app_name="core"
 
 urlpatterns=[
     path("",views.welcome,name="welcome"),
+    path('send-message/', views.send_message, name='send_message'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,62 @@
 from django.shortcuts import render
 from core.models import TeamModel
 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+import os
+
+# chatbot
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+# Load environment variables
+load_dotenv()
+
+# Configure Google Gemini Pro API
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Prompt for the GenerativeModel
+prompt = """You are the safety advisor specialized in forest fires, wildfires, and disaster management. 
+Provide essential tips and information on prevention, safety protocols during emergencies. 
+Concise tips within 250 words. Answer questions related to fire safety and disaster management."""
+
+# Function to generate content using Gemini Pro
+def generate_gemini_content(transcript_text, prompt):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt + transcript_text)
+    return response.text
+
+# Streamlit UI
+st.title("WildFireCatch 💨🔥 Ask your disaster-related issues and tips")
+text = st.text_area("Enter Your Transcript Text:")
+
+if st.button("Get Detailed Notes"):
+    if text:
+        summary = generate_gemini_content(text, prompt)
+        st.markdown("## Detailed Notes:")
+        st.write(summary)
+    else:
+        st.warning("Please enter some text to generate detailed notes.")
+
+
+
 def welcome(request):
     teamdetails=TeamModel.objects.all()
     context={
         'teamdetails':teamdetails,
     }
     return render(request,'core/app/welcome.html',context)
+
+@csrf_exempt
+def send_message(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user_message = data.get('message', '')
+        if user_message:
+            response_message = generate_gemini_content(user_message, prompt)
+            return JsonResponse({"reply": response_message})
+        return JsonResponse({"reply": "I didn't understand that. Could you please rephrase?"})
+    return JsonResponse({"error": "Invalid request method."}, status=405)

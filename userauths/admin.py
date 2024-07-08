b31b2c082ef997a032f7d9dd014598e068fd60e7
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-08 08:21:21 +0530

    resolved error

--- userauths/admin.py
+++ userauths/admin.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from userauths.models import User
 from userauths.models import Profile
 
-from import_export.admin import ImportExportModelAdmin
 
 class UserAdmin(admin.ModelAdmin):
     list_display=['username','email','bio']
 admin.site.register(User,UserAdmin)
 
 
-class ProfileAdmin(ImportExportModelAdmin):
+class ProfileAdmin():
     list_display=['user']
 admin.site.register(Profile,ProfileAdmin)

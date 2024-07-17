bd144c308dcd78fad4fe43493fb9edfb941c1ac6
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-18 02:04:13 +0530

    updation for mac
    updated for mac and fixed problems for mac

--- wildfire/settings.py
+++ wildfire/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
